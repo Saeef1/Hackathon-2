de5deadf6209c805fd45d4dcc3dde3b5418ce33c
Muhammad Saeef <160288809+Saeef1@users.noreply.github.com>
2024-12-22 16:03:39 -0800

    Update section2.tsx

--- src/app/Product/section2.tsx
+++ src/app/Product/section2.tsx
@@ -10,7 +10,7 @@ export default function Section2() {
 
             <div className="sm:w-[1039px] w-[100%] sm:h-[174px] h-auto flex gap-6 px-4 sm:px-0 flex-col justify-between">
                 <p>
-                    Embodying the raw, wayward spirit of rock 'n' roll, the Kilburn portable active stereo speaker takes the unmistakable look and sound of Marshall, unplugs the chords, and takes the show on the road.
+                    Embodying the raw, wayward spirit of rock &lsquon&lsquo roll, the Kilburn portable active stereo speaker takes the unmistakable look and sound of Marshall, unplugs the chords, and takes the show on the road.
                 </p>
                 <p>
                     Weighing in under 7 pounds, the Kilburn is a lightweight piece of vintage styled engineering. Setting the bar as one of the loudest speakers in its class, the Kilburn is a compact, stout-hearted hero with a well-balanced audio which boasts a clear midrange and extended highs for a sound that is both articulate and pronounced. The analogue knobs allow you to fine tune the controls to your personal preferences while the guitar-influenced leather strap enables easy and stylish travel.
